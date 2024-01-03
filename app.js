bdcde4d51dfc074284db3f961178a23c7968ea18
Uğur ABDİL <141768320+ugurabdil@users.noreply.github.com>
2024-01-03 21:29:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const panels=document.querySelectorAll(".panel");
+
+panels.forEach(panel=>{
+    panel.addEventListener("click",()=>{
+        removeActive();
+        panel.classList.add("active");
+    });
+})
+function removeActive(){
+    panels.forEach(panel=>{
+        panel.classList.remove("active");
+    })
+}
+
+
+// const panels = document.querySelectorAll(".panel");
+
+// panels.forEach(panel => {
+//     panel.addEventListener("click", () => {
+
+//         removeActive();
+//         panel.classList.add("active");
+//     });
+// })
+
+// function removeActive(){
+//     panels.forEach(panel => {
+//         panel.classList.remove("active");
+//     })
+// }
